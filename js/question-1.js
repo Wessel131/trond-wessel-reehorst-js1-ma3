b2b26def300051912535424f1472219d44fd0574
Wessel131 <83341367+Wessel131@users.noreply.github.com>
2021-11-05 14:20:05 +0100

    finished
    final final

---
+++ js/question-1.js
@@ -0,0 +1,5 @@
+// Task 1
+console.log("Task 1");
+const getRemainder = (a, b) => a % b;
+
+console.log("Remainder: ", getRemainder(10, 8));
--- js/question-2.js
+++ js/question-2.js
@@ -1,9 +1,3 @@
-// Task 1
-console.log("Task 1");
-const getRemainder = (a, b) => a % b;
-
-console.log("Remainder: ", getRemainder(10, 8));
-
 // Task 2
 console.log("Task 2");
 
